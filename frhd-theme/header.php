999ebba1970d82faf2366f0afeb54fb4a9dbee36
Fernando Putallaz <ferhedio@gmail.com>
2025-03-20 18:53:22 -0300

    add logo link

--- frhd-theme/header.php
+++ frhd-theme/header.php
@@ -1,4 +1,9 @@
 <header>
+    <div class="site-branding">
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="custom-logo-link" rel="home">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/logo.png" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="custom-logo">
+        </a>
+    </div>
     <nav>
         <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
     </nav>
